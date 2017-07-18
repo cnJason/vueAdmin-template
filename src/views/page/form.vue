8b0de09b397e94db2ed9dc4d363169ad3726e6b8
cnJason <chriscyx@vip.qq.com>
2017-07-18 17:57:12 +0800

    modify file format

--- src/views/page/form.vue
+++ src/views/page/form.vue
@@ -1,72 +1,146 @@
 <template>
     <div class="app-container">
-        <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="活动名称">
-                <el-input v-model="form.name"></el-input>
+        <el-form :model="saveReport" ref="saveReport" label-width="100px">
+            <el-form-item prop="environment" label="测试环境">
+                <el-input v-model="saveReport.environment"></el-input>
             </el-form-item>
-            <el-form-item label="活动区域">
-                <el-select v-model="form.region" placeholder="请选择活动区域">
-                    <el-option label="区域一" value="shanghai"></el-option>
-                    <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
+            <el-form-item prop="environment" label="测试工具">
+                <el-input v-model="saveReport.tool"></el-input>
             </el-form-item>
-            <el-form-item label="活动时间">
-                <el-col :span="11">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                </el-col>
-                <el-col class="line" :span="2">-</el-col>
-                <el-col :span="11">
-                    <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                </el-col>
-            </el-form-item>
-            <el-form-item label="即时配送">
-                <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
-            </el-form-item>
-            <el-form-item label="活动性质">
-                <el-checkbox-group v-model="form.type">
-                    <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                    <el-checkbox label="地推活动" name="type"></el-checkbox>
-                    <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                    <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                </el-checkbox-group>
-            </el-form-item>
-            <el-form-item label="特殊资源">
-                <el-radio-group v-model="form.resource">
-                    <el-radio label="线上品牌商赞助"></el-radio>
-                    <el-radio label="线下场地免费"></el-radio>
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="活动形式">
-                <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form>
+        <el-form :model="saveReport" ref="saveReport" :inline="true" v-for="(item, index) in saveReport.items">
+            <el-form-item
+                    :label="'功能点'"
+                    :key="item.point"
+                    :prop="'items.' + index + '.point'">
+                <el-input v-model="item.point"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                <el-button>取消</el-button>
+                <el-button @click="addIndexItem(index)">新增测试项</el-button>
+                <el-button @click="deleteItem(item)">删除功能点</el-button>
+            </el-form-item>
+            <div v-for="(subItem, subIndex) in saveItems" :key="subIndex">
+                <div v-if="subItem.pIndex === index">
+                    <el-form-item
+                            :label="'测试项'"
+                            :key="item.testItem"
+                            :prop="'saveItems.' + index + '.testItem'">
+                        <el-input v-model="item.testItem"></el-input>
+                    </el-form-item>
+                    <el-form-item :label="'类型'"
+                                  :key="item.type"
+                                  :prop="'saveItems.' + index + '.type'">
+                        <el-select v-model="item.type" placeholder="类型">
+                            <el-option
+                                    v-for="item in typeOption"
+                                    :key="item.value"
+                                    :label="item.value"
+                                    :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item :label="'风险等级'"
+                                  :key="item.level"
+                                  :prop="'saveItems.' + index + '.level'">
+                        <el-select v-model="item.level" placeholder="风险等级">
+                            <el-option
+                                    v-for="level in levelOption"
+                                    :key="level.value"
+                                    :label="level.value"
+                                    :value="level.value">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item :label="'评测结果'"
+                                  :key="item.result"
+                                  :prop="'saveItems.' + index + '.result'">
+                        <el-select v-model="item.result" placeholder="评测结果">
+                            <el-option
+                                    v-for="result in resultOption"
+                                    :key="result.value"
+                                    :label="result.value"
+                                    :value="result.value">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-button @click="deleteIndexItem(subItem)">删除测试项</el-button>
+                </div>
+            </div>
+        </el-form>
+        <el-form :model="saveReport" ref="saveReport" label-width="100px">
+            <el-form-item>
+                <el-button @click="addItem()">新增功能点</el-button>
             </el-form-item>
         </el-form>
     </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+    import ElForm from "../../../node_modules/element-ui/packages/form/src/form.vue";
+
+    export default {
+        components: {ElForm},
+        data() {
+            return {
+                saveReport: {
+                    environment: '',
+                    tool: '',
+                    items: []
+                },
+                saveItems: [],
+                typeOption: [{
+                    label: '服务器逻辑'
+                }, {
+                    label: '本地逻辑'
+                }, {
+                    label: '数据安全'
+                }],
+                levelOption: [{
+                    label: ' 高'
+                }, {
+                    label: '中'
+                }, {
+                    label: '低'
+                }],
+                resultOption: [{
+                    label: ' 高危'
+                }, {
+                    label: '低风险'
+                }, {
+                    label: '安全'
+                }]
+            }
+        },
+        methods: {
+            addItem() {
+                this.saveReport.items.push({
+                    point: '',
+                    key: Date.now()
+                })
+                console.log(this.saveReport.items.length);
+            },
+            deleteItem(item){
+                var index = this.saveReport.items.indexOf(item)
+                if (index !== -1) {
+                    this.saveReport.items.splice(index, 1)
+                }
+            },
+            deleteIndexItem(subItem){
+                var subIndex = this.saveItems.indexOf(subItem)
+                if (subIndex !== -1) {
+                    this.saveItems.splice(subIndex, 1)
+                }
+            },
+            addIndexItem(index) {
+                this.saveItems.push({
+                    testItem: '',
+                    type: '',
+                    level: '',
+                    result: '',
+                    pIndex: index,
+                    key: Date.now()
+                })
+            }
         }
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
-      }
     }
-  }
 </script>
